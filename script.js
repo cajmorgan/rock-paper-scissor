c104b505f33b2489b5701abaaade1403a79e39c9
Caj Morgan <caj@cajmorgan.com>
2021-04-28 14:32:04 +0200

    Working console version of rock/paper/scissors

---
+++ script.js
@@ -0,0 +1,101 @@
+var countP = 0;
+var countC = 0;
+var num = 0;
+
+function computerPlay(min = 1, max = 3) {
+    let randomNum = Math.floor(Math.random() * (max - min + 1) + min); 
+    switch(randomNum) {
+        case 1:
+            return 'rock';    
+        case 2:
+            return 'paper';
+        case 3: 
+            return 'scissors';
+        default:
+            break;
+    }
+    
+    return randomNum;
+    
+}
+
+function letsPlay() {
+    message = "";
+    let playerSelection = prompt('enter: rock, paper or scissors');
+    while(playerSelection.toLowerCase() != 'rock' && playerSelection.toLowerCase() != 'paper' && playerSelection.toLocaleLowerCase() != 'scissors' ) {
+         playerSelection = prompt('Check your spelling! enter: rock, paper or scissors');
+    };
+
+    let computerSelection = computerPlay();
+     
+    if (playerSelection.toLowerCase() == computerSelection) {
+        countC += 1
+        countP += 1
+        scoreAdder()
+        let result = ((num == 3) ? message = "The End" : message = "It's a tie!");
+        return message;
+    }
+    else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper') {
+        countC += 1
+        scoreAdder()
+        let result = ((num == 3) ? message = "The End" : message = "You lost! paper beats rock!");
+        return message;
+    }
+    else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors') {
+        countP += 1
+        scoreAdder()
+        let result = ((num == 3) ? message = "The End" : message = "You won! rock beats scissors");
+        return message;
+    }
+    else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'scissors') {
+        countC += 1
+        scoreAdder()
+        let result = ((num == 3) ? message = "The End" : message = "You lost! scissors beats paper!");
+        return message;
+    }
+    else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock') {
+        countP += 1
+        scoreAdder()
+        let result = ((num == 3) ? message = "The End" : message = "You won! paper beats rock!");
+        return message;
+    }
+    else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'rock') {
+        countC += 1
+        scoreAdder()
+        let result = ((num == 3) ? message = "The End" : message = "You lost! rock beats scissors");
+        return message;
+    }
+    else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper') {
+        countP += 1
+        scoreAdder()
+        let result = ((num == 3) ? message = "The End" : message = "You won! scissors beats paper");
+        return message;
+    }
+   
+}
+
+function gameRounds() {
+    while(num < 3) {
+        num += 1;
+        console.log(letsPlay())
+    }
+}
+
+gameRounds();
+
+
+function scoreAdder() {
+    if (num == 3 && countC > countP) {
+        counter = (`Final Score: You Lost, Computer ${countC} vs you ${countP}`);
+    }
+    else if (num == 3 && countP > countC) {
+        counter = (`Final Score: You Won, You ${countP} vs Computer ${countC}`);
+    }
+    else if (num == 3 && countP == countC) {
+        counter = (`Final Score: It's a tie! You ${countP} vs Computer ${countC}`);
+    }
+    else {
+        counter = (`Score: Computer ${countC} vs you ${countP}`)
+    }
+    return console.log(counter)
+};
